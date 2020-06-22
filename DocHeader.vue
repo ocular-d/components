0b83295e4284361574bcbca885e604e49cf20173
sven <sven@so36.net>
2020-06-22 17:13:11 +0200

    components(docheader): wip v 0.2

--- DocHeader.vue
+++ DocHeader.vue
@@ -5,5 +5,8 @@
       style="width: 4rem; height: .5rem; margin: .1rem 0rem 2rem .3rem; background-color: #5469d4;"
     ></div>
     <h4>{{$page.frontmatter.description}}</h4>
+    <br />
+    &#x1F4A1; {{ $page.readingTime.text }}
+
   </div>
 </template>
