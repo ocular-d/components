52419a4d79fe7a62f5fa3cb24181f9bbbb44dcd9
sven <sven@so36.net>
2020-10-16 11:47:06 +0200

    add headline component

---
+++ vp-test/docs/.vuepress/components/Headline.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="relative mt-8 mb-4">
+        <h1
+          class="text-3xl font-extrabold leading-8 tracking-tight text-center text-gray-900 sm:text-4xl sm:leading-10"
+        >{{$page.frontmatter.headline.text}}</h1>
+        <p
+          v-if="$page.frontmatter.headline.subtext"
+          class="max-w-3xl mx-auto mt-4 text-xl leading-7 text-center text-gray-500"
+        >{{$page.frontmatter.headline.subtext}}</p>
+      </div>
+      <div class="py-8 mb-8 text-center" v-if="$page.frontmatter.headline.pricingLinkText">
+        <a href="#pricing" class="text-green-500 hover:underline">{{$page.frontmatter.headline.pricingLinkText}}</a>
+      </div>
+  </div>
+</template>
---
+++ vp-test/docs/foo.md
@@ -0,0 +1,13 @@
+---
+headline: {
+  text: Amazon Seller Tool,
+  subtext: Please find an overview on most relevant features below,
+  pricingLinkText: Go directly to our pricing overview
+}
+---
+
+# Foo
+
+<Headline />
+
+<p style="color:blue;">I am blue</p>
