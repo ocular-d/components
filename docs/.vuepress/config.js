071e93779752aa59bebbef189273181a547c535a
sven <sven@so36.net>
2020-12-17 11:21:31 +0100

    vuepress(theme): move to ocld style

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,17 @@ module.exports = {
   title: 'VuePress Components',
   description: 'Read my description ...',
   head: [
+    ['link', { rel: 'icon', href: '/favicon.png' }],
+    ['link', {
+      rel: 'stylesheet',
+      type: 'text/css',
+      href: 'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,800,800i,900,900i'
+  },],
+  ['link', {
+    rel: 'stylesheet',
+    type: 'text/css',
+    href: 'https://fonts.googleapis.com/css?family=Miriam+Libre:400,700'
+},],
     ['script', { src: 'https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js'}]
   ],
 
--- docs/beta-banner.md
+++ docs/beta-banner.md
@@ -1,5 +1,26 @@
 ---
-title: Beta Banner
+title: Banner
+description: A banner component.
+meta:
+  - name: "og:description"
+    content: A Banner component
+  - name: keywords
+    content: A banner component.
+tags:
+  - Banner
+
 ---
 
+<Header/>
+
+---
+
+# Example
+
 <BetaBanner/>
+
+## Usage
+
+```markdown
+<BetaBanner/>
+```
